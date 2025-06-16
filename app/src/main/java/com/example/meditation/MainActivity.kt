e0635b67c26a83ad63f63d3f48e74fc5175ca0f5
mahendra <gohilmahendra424@gmail.com>
2025-06-16 17:09:54 +0530

    all_hi

--- app/src/main/java/com/example/meditation/MainActivity.kt
+++ app/src/main/java/com/example/meditation/MainActivity.kt
@@ -4,73 +4,95 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StampedPathEffectStyle
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.meditation.ui.theme.customAppFontFamily
+import com.example.meditation.ui.theme.homepagecolor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                topBar = { MytitalBar() }
-            ) { innerPadding ->
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding)
-                )
-                {
-                    MyHomepage()
-                }
-            }
+            MyHomepage()
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun MytitalBar() {
-        TopAppBar(
-            title = {
-                Text(
-                    text = "Meditation",
-                    fontSize = 25.sp,
-                    fontFamily = customAppFontFamily
-                )
-            },
-            navigationIcon = {
-                IconButton(onClick = {
-                    finish()
-                })
-                {
-                    Icon(
-                        Icons.Default.ExitToApp,
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-            }
-        )
-    }
-
-    @Composable
+    @Preview
     fun MyHomepage() {
-                
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(R.drawable.meditation_man),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+            IconButton(onClick = {}, modifier = Modifier.padding(top = 40.dp, start = 5.dp))
+            {
+                Icon(
+                    Icons.Default.ExitToApp,
+                    contentDescription = null,
+                    modifier = Modifier.size(23.dp),
+                    tint = Color.Black
+                )
+            }
+            Text(
+                text = "Meditation",
+                fontSize = 20.sp,
+                color = Color.Black,
+                fontFamily = customAppFontFamily,
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(top = 55.dp)
+            )
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(bottom = 57.dp, start = 35.dp)
+                    .width(135.dp)
+                    .height(45.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                border = BorderStroke(1.dp, color = Color.White)
+            ) {
+                Text(
+                    text = "Start",
+                    fontFamily = customAppFontFamily,
+                    color = Color.White,
+                    fontSize = 19.sp
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/meditation/ui/theme/Color.kt
+++ app/src/main/java/com/example/meditation/ui/theme/Color.kt
@@ -10,4 +10,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 val chichu = Color(202, 215, 224)
+val homepagecolor = Color(54, 54, 54)
 
